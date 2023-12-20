8f7f4e407b575c8afecd4c212e51d127d369cf49
Hilmi KALE <hilmikale3139@gmail.com>
2023-12-20 17:32:56 +0300

    data is fetched

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+import axios from "axios";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(url);
+      setTours(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  useEffect(() => {
+    if (show) {
+      fetchTours();
+    }
+  }, [show]);
+
+  if (!show) {
+    return (
+      <main>
+        <div className="title">
+          <h2>Welcome to the tours</h2>
+          <button className="btn" onClick={() => setShow(true)}>
+            Show Tours
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 }
 
