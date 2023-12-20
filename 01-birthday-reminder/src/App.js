5cf505a65777723eef9e7a1b21865c386dbdc42e
Hilmi KALE <hilmikale3139@gmail.com>
2023-12-20 15:04:51 +0300

    app js and list js created

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+import "./App.css";
 
 function App() {
+  const people = data;
+  const [showList, setShowList] = useState(false);
+
+  const handleShowClick = () => {
+    setShowList(true);
+  };
+  const handleClearClick = () => {
+    setShowList(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        {/* {showList && <h3>{people.length} birthdays today</h3>}
+        {showList && <List people={people} />}
+        {showList ? (
+          <button onClick={handleClearClick}>Clear All</button>
+        ) : (
+          <button onClick={handleShowClick}>Show</button>
+        )} */}
+        {showList ? (
+          <>
+            <h3>{people.length} birthdays today</h3>
+            <List people={people} />
+            <button onClick={handleClearClick}>Clear All</button>
+          </>
+        ) : (
+          <button onClick={handleShowClick}>Show Birthdays</button>
+        )}
+      </section>
+    </main>
   );
 }
 
