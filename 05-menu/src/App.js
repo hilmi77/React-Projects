afdf28604874b7156c485fc90ef4b7cf7129cde0
Hilmi KALE <hilmikale3139@gmail.com>
2023-12-22 16:05:40 +0300

    show button added

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from "react";
+// import Menu from "./Menu";
+// import Categories from "./Categories";
+// import items from "./data";
+// const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+// function App() {
+//   const [menuItems, setMenuItems] = useState(items);
+//   const [categories, setCategories] = useState(allCategories);
+
+//   const filterItems = (category) => {
+//     if (category === "all") {
+//       setMenuItems(items);
+//       return;
+//     }
+//     const newItems = items.filter((item) => item.category === category);
+//     setMenuItems(newItems);
+//   };
+
+//   return (
+//     <main>
+//       <section className="menu-section">
+//         <div className="title">
+//           <h2>Our Menu</h2>
+//           <div className="underline"></div>
+//         </div>
+//         <Categories categories={categories} filterItems={filterItems} />
+//         <Menu items={menuItems} />
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default App;
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+  const [show, setShow] = useState(false);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const NewItems = items.filter((item) => item.category === category);
+    setMenuItems(NewItems);
+  };
+
+  const showMenu = () => {
+    setShow(!show);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu-section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+          <button className=" show-btn" onClick={showMenu}>
+            {show ? "Hide Menu" : "Show Menu"}
+          </button>
+        </div>
+
+        {show && (
+          <>
+            <Categories categories={categories} filterItems={filterItems} />
+            <Menu items={menuItems} />{" "}
+          </>
+        )}
+      </section>
+    </main>
   );
 }
 
