5ffb3cb6cf6a673fe5c0f6424da7e52526f09a9e
Hilmi KALE <hilmikale3139@gmail.com>
2023-12-30 16:32:03 +0300

    color generator component created

--- 09-color-generator/src/App.tsx
+++ 09-color-generator/src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Values from "values.js";
+type Color = {
+  rgb: number[];
+  weight: number;
+  hex: string;
+};
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+  const [list, setList] = useState<Color[]>(new Values("#f15025").all(10));
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          const { rgb, weight, hex } = color;
+          return (
+            <article
+              key={index}
+              className="color"
+              style={{ backgroundColor: `rgb(${rgb.join(",")})` }}
+            >
+              <p className="percent-value">{weight}%</p>
+              <p className="color-value">{hex}</p>
+            </article>
+          );
+        })}
+      </section>
+    </>
   );
-}
+};
 
 export default App;
