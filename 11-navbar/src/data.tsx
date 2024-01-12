6c1f32af2258d62144cf957b0d678e004dc59775
Hilmi KALE <hilmikale3139@gmail.com>
2024-01-12 15:54:15 +0300

    navbar created

---
+++ 11-navbar/src/data.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+interface LinkItem {
+  id: number;
+  url: string;
+  text: string;
+}
+interface SocialItem {
+  id: number;
+  url: string;
+  icon: JSX.Element;
+}
+export const links: LinkItem[] = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "projects",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    id: 5,
+    url: "/profile",
+    text: "profile",
+  },
+];
+
+export const social: SocialItem[] = [
+  {
+    id: 1,
+    url: "https://www.facebook.com",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.linkedin.com",
+    icon: <FaLinkedin />,
+  },
+];
