19394f525ebc33b16c4f078377941b434968fdc6
Hilmi KALE <hilmikale3139@gmail.com>
2024-01-13 11:05:49 +0300

    home created

---
+++ 12-sidebar-modal/src/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context/context";
+
+const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        Show Modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
