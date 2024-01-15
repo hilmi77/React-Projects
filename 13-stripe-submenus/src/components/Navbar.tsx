e68d345bcf3adc55b6256294410ce840da5b7427
Hilmi KALE <hilmikale3139@gmail.com>
2024-01-15 16:22:19 +0300

    logo added

--- 13-stripe-submenus/src/components/Navbar.tsx
+++ 13-stripe-submenus/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./images/logo.svg";
+import logo from "../images/logo.svg";
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "../context/context";
 
