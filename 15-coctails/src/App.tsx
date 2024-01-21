f54761f1ec5cd00f698915348fcd4198df841534
Hilmi KALE <hilmikale3139@gmail.com>
2024-01-21 13:31:40 +0300

    react-router-dom installed

--- 15-coctails/src/App.tsx
+++ 15-coctails/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCoctail from "./pages/SingleCoctail";
+import Error from "./pages/Error";
 
-function App() {
+type Props = {};
+
+const App = (props: Props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCoctail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
