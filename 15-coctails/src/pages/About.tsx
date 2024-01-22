f0c332257f94bd57fc43c77614b6ae5b65c424df
Hilmi KALE <hilmikale3139@gmail.com>
2024-01-22 15:23:34 +0300

    about and navbar created

---
+++ 15-coctails/src/pages/About.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae cumque
+        veniam facere dignissimos nemo perspiciatis mollitia optio quisquam iste
+        voluptatum maxime, esse explicabo ullam cum totam tempora porro quaerat
+        corrupti dolores nostrum eum rerum harum, rem ipsa! Commodi, officiis!
+      </p>
+    </section>
+  );
+};
+
+export default About;
